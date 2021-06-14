91e42a3254b8a90cdd48e8890fa5c7f955cfff39
Christopher Hex <christopher.hex@extrahorizon.com>
2021-06-14 16:04:06 +0200

    Add extra logs

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,6 +2,7 @@
 
 echo "Markdown to Confluence"
 
+echo "Going to add $MARKDOWN_FILE to $CONFLUENCE_SPACE via url $CONFLUENCE_API_URL"
 python $SCRIPT_DIRECTORY/markdown-to-confluence.py \
   --api_url $CONFLUENCE_API_URL \
   --username $CONFLUENCE_USER \
